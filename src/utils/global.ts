0ea1baccc546c61ea5e920acadfd186091739954
João Lenon <lenonsec7@gmail.com>
2021-11-03 12:21:45 -0300

    fix: Debug function with context and namespace

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -7,14 +7,21 @@ import { DebugTransporter } from '../Transporters/DebugTransporter'
 export {}
 
 declare global {
+  function Debug(message: any, namespace?: string, context?: string): void
   function Log(message: any, formatterOpts?: any, transporterOpts?: any): void
-  function Debug(message: any, formatterOpts?: any, transporterOpts?: any): void
 }
 
 const _global = global as any
-
-_global.Log = Log
-_global.Debug = new Logger().debug.bind({
+const debug = new Logger().debug.bind({
   context: 'Debugger',
   mapper: new LogMapper([new DebugFormatter()], [new DebugTransporter()]),
 })
+
+_global.Log = Log
+_global.Debug = (
+  message: any,
+  namespace = 'api:main',
+  context = 'Debug',
+): void => {
+  debug(message, { context }, { namespace })
+}
